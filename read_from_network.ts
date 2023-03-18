6fcb53a99d45c9957020affadae355818097248e
chrshdl <cwasilei@gmail.com>
2023-03-18 16:05:13 +0100

    feat: add sendBalance support

--- read_from_network.ts
+++ read_from_network.ts
@@ -1,4 +1,4 @@
-import { Keypair, PublicKey, Connection, clusterApiUrl, LAMPORTS_PER_SOL } from "@solana/web3.js";
+import { Keypair, PublicKey, Connection, clusterApiUrl, LAMPORTS_PER_SOL, SystemProgram, Transaction, sendAndConfirmTransaction } from "@solana/web3.js";
 import { readFileSync } from "fs";
 import { homedir } from 'os'
 
@@ -6,15 +6,52 @@ const secret = JSON.parse(readFileSync(homedir() + "/.config/solana/id.json").to
 const secretKey = Uint8Array.from(secret)
 const myKeypair = Keypair.fromSecretKey(secretKey)
 
-const myPublicKey = myKeypair.publicKey
+const connection = new Connection(clusterApiUrl("devnet"))
 
-
-
+/**
+ * 
+ * @param address 
+ * @returns 
+ */
 async function getBalance(address: PublicKey): Promise<number> {
-    const connection = new Connection(clusterApiUrl("devnet"))
-    return connection.getBalance(address)
-  }
+  return connection.getBalance(address)
+}
+
+
+/**
+ * 
+ * @param fromAddress 
+ * @param toAddress 
+ * @param solana 
+ * @returns 
+ */
+async function sendBalance(fromAddress: PublicKey, toAddress:PublicKey, solana:number): Promise<string> {
   
-  getBalance(new PublicKey(myPublicKey.toBase58())).then((balance) => {
-    console.log(balance / LAMPORTS_PER_SOL + " SOL");
+  const transaction = new Transaction()
+
+  const sendSolanaInstruction = SystemProgram.transfer({
+    fromPubkey: fromAddress,
+    toPubkey: toAddress,
+    lamports: LAMPORTS_PER_SOL * solana
   })
+
+  transaction.add(sendSolanaInstruction)
+
+  const signature = await sendAndConfirmTransaction(
+    connection,
+    transaction,
+    [myKeypair]
+  )
+
+  return signature
+}
+
+getBalance(myKeypair.publicKey).then((balance) => {
+  console.log(balance / LAMPORTS_PER_SOL + " SOL")
+})
+
+sendBalance(
+  myKeypair.publicKey,
+  new PublicKey("7C4jsPZpht42Tw6MjXWF56Q5RQUocjBBmciEjDa8HRtp"), 0.002).then((signature) => {
+  console.log(signature)
+})
